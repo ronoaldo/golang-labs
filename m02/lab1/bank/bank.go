aff313727c55b24e369d5788ec2129bf8ee7382c
Ronoaldo JLP <ronoaldo@gmail.com>
2023-06-25 17:09:50 -0300

    feat: tweaked m02/lab1 output and correctness.
    Humanized float number and used the Log function
    from previous exercise.

--- m02/lab1/bank/bank.go
+++ m02/lab1/bank/bank.go
@@ -3,11 +3,13 @@ package bank
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"strings"
+	"time"
 )
 
 var (
-	ErrNegativeBallance = errors.New("bank: ballance bellow zero.")
+	ErrNegativeBallance = errors.New("bank: ballance bellow zero")
 )
 
 type Account interface {
@@ -19,19 +21,41 @@ type Account interface {
 type account float64
 
 func (a *account) Deposit(reason string, value float64) {
+	logOp("DEPOSIT", reason, float64(*a), value)
 	*a += account(value)
 }
 
 func (a *account) Withdraw(reason string, value float64) {
+	logOp("WITHDRAW", reason, float64(*a), -value)
 	*a -= account(value)
 }
 
+// https://go.dev/play/p/5TJveEXzJ9m
+// This is not a performant aproach, using []byte instead of string
+// performs better.
+func (a *account) humanize() string {
+	// Round and split integer/fraction parts
+	aux := fmt.Sprintf("%.02f", *a)
+	in, frac := strings.Split(aux, ".")[0], strings.Split(aux, ".")[1]
+
+	// From end to first, add a '.' each 3 digits
+	aux = ""
+	i := len(in)
+	for ; i > 3; i -= 3 {
+		aux = "." + in[i-3:i] + aux
+	}
+	// Add remaining digits at the begining
+	aux = in[:i] + aux
+
+	return aux + "," + frac
+}
+
 func (a *account) String() string {
-	out := fmt.Sprintf("Balance R$ %.02f", float64(*a))
-	return strings.Replace(out, ".", ",", -1)
+	return "Balance R$ " + a.humanize()
 }
 
 func (a *account) Close() error {
+	logOp("CLOSE", "Account closing", float64(*a), 0)
 	if *a < 0 {
 		return ErrNegativeBallance
 	}
@@ -39,6 +63,25 @@ func (a *account) Close() error {
 }
 
 func NewAccount() Account {
+	logOp("OPEN", "Account openning", 0, 0)
 	var a account
 	return &a
+}
+
+func fstr(f float64) string {
+	return strconv.FormatFloat(f, 'f', 2, 64)
+}
+
+func logOp(op, desc string, prevBallance, value float64) {
+	hhmm := time.Now().Format("03:04")
+	ballance := prevBallance + value
+	fmt.Printf(
+		"%s %-6.6s %-12.12s %8s %8s %8s\n",
+		hhmm,
+		op,
+		desc,
+		fstr(prevBallance),
+		fstr(value),
+		fstr(ballance),
+	)
 }
--- m02/lab1/main.go
+++ m02/lab1/main.go
@@ -9,16 +9,16 @@ func main() {
 	a := bank.NewAccount()
 	a.Deposit("Abertura de conta", 100.00)
 	a.Withdraw("Compra no débito", 50.00)
-	fmt.Printf("%v\n", a)
+	fmt.Printf("> %v\n", a)
 	a.Withdraw("Compra no débito", 100.00)
-	fmt.Printf("%v\n", a)
+	fmt.Printf("> %v\n", a)
 
 	if err := a.Close(); err != nil {
-		fmt.Printf("Error: %v\n", err)
+		fmt.Printf("> Error: %v\n", err)
 	}
 
 	a.Deposit("Encerramento de conta", 50.00)
 	if err := a.Close(); err != nil {
-		fmt.Printf("Error: %v\n", err)
+		fmt.Printf("> Error: %v\n", err)
 	}
 }
